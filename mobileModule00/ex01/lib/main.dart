6e94cc8d08da89ecc82649857425ed32032110f9
Sergio Ternero <stcshooter@gmail.com>
2026-06-12 13:55:06 +0200

    piscine_pedago_mobile

--- mobileModule00/ex01/lib/main.dart
+++ mobileModule00/ex01/lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Quita la cinta de debug
       home: Scaffold(
-        appBar: AppBar(title: const Text("ex01: Say Hello!")),
-        // debugShowCheckedModeBanner: false, // Quita la cinta de debug
+        // appBar: AppBar(title: const Text("ex01: Say Hello!")),
         body: const MyStatefulWidget(),
-      ),
+        ),
     );
   }
 }
